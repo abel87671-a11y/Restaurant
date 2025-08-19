0c532f5d4a253fa67b0a41344c3c3bb83f66dd0e
abel87671-a11y <abel87671@gmail.com>
2025-08-19 13:11:08 +0530

    code push

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+AOS.init();
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4.5,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
